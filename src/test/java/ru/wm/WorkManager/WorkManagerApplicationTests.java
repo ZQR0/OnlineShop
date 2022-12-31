a736e290aa377e51c9e4dcb4f1d63a691dc41593
ZQR0 <yarikrech@gmail.com>
2022-12-31 17:44:48 +0300

    init repo

---
+++ src/test/java/ru/wm/WorkManager/WorkManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.wm.WorkManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

0a69bbd86d607229a58a3c46bba6aac4a8db98cb
ZQR0 <yarikrech@gmail.com>
2023-01-02 22:17:58 +0300

    Password Encoder Config is ready

---
+++ src/main/java/ru/wm/WorkManager/config/PasswordEncoderConfig.java
@@ -0,0 +1,25 @@
+package ru.wm.WorkManager.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/*
+* @author ZQR0
+* @since 02.01.2023
+* Password encoder config for create a cryptography operations
+*/
+
+@Configuration
+public class PasswordEncoderConfig {
+
+    /*
+    * @method
+    */
+    @Bean(name = "bcrypt_bean")
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}

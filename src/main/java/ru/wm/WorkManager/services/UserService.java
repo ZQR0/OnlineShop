f0ffd3a7ebbb65e8f03525ca2f8259427fb4fc2b
ZQR0 <yarikrech@gmail.com>
2023-01-13 23:51:17 +0300

    UserService register method and email validate method added

--- src/main/java/ru/wm/WorkManager/services/UserService.java
+++ src/main/java/ru/wm/WorkManager/services/UserService.java
@@ -1,17 +1,22 @@
 package ru.wm.WorkManager.services;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.validator.routines.EmailValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import ru.wm.WorkManager.dto.UserDTO;
 import ru.wm.WorkManager.entities.UserEntity;
+import ru.wm.WorkManager.exceptions.EmailValidationException;
+import ru.wm.WorkManager.exceptions.UserNotFoundException;
 import ru.wm.WorkManager.repositories.UserRepository;
 import ru.wm.WorkManager.services.interfaces.UserServiceInterface;
 import ru.wm.WorkManager.utils.DateProvider;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.logging.Logger;
 
 @Service
 @Slf4j
@@ -42,12 +47,34 @@ public class UserService implements UserServiceInterface {
     }
 
     @Override
-    public UserEntity findByUsername(String username) {
+    public UserEntity findByUsername(String username) throws UsernameNotFoundException {
         return this.repository.findByUsername(username);
     }
 
     @Override
-    public void register(UserDTO dto) {
-        // implementation soon
+    public void register(UserDTO dto) throws EmailValidationException {
+
+        if (validateEmail(dto.getEmail())) {
+            this.repository.save(
+                    UserEntity.builder()
+                            .setEmail(dto.getEmail())
+                            .setUsername(dto.getUsername())
+                            .setPassword(dto.getPassword())
+                            .date(this.dateProvider.now())
+                            .build()
+            );
+
+            Logger.getLogger("registerUser").info("User is registered");
+        }
+
+        Logger.getLogger("registerUser").info("User isn't registered");
+
+        throw new EmailValidationException("Email validation failed");
+    }
+
+    private boolean validateEmail(String email) {
+        // Got this pattern from : https://www.baeldung.com/java-email-validation-regex
+        //final String regexPattern = "^(?=.{1,64}@)[A-Za-z0-9\\\\+_-]+(\\\\.[A-Za-z0-9\\\\+_-]+)*@" + "[^-][A-Za-z0-9\\\\+-]+(\\\\.[A-Za-z0-9\\\\+-]+)*(\\\\.[A-Za-z]{2,})$";
+        return EmailValidator.getInstance().isValid(email);
     }
 }

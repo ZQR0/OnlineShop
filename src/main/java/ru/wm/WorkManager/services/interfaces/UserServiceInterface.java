3272a99028f66675a22eed079e3058922a01249a
ZQR0 <yarikrech@gmail.com>
2023-01-13 23:50:07 +0300

    UserServiceInterface exceptions added to some methods

--- src/main/java/ru/wm/WorkManager/services/interfaces/UserServiceInterface.java
+++ src/main/java/ru/wm/WorkManager/services/interfaces/UserServiceInterface.java
@@ -1,7 +1,10 @@
 package ru.wm.WorkManager.services.interfaces;
 
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import ru.wm.WorkManager.dto.UserDTO;
 import ru.wm.WorkManager.entities.UserEntity;
+import ru.wm.WorkManager.exceptions.EmailValidationException;
+import ru.wm.WorkManager.exceptions.UserNotFoundException;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,7 +19,7 @@ import java.util.Optional;
 public interface UserServiceInterface {
     Optional<UserEntity> findById(Long id);
     List<UserEntity> getAllUsers();
-    void register(UserDTO dto);
+    void register(UserDTO dto) throws EmailValidationException;
     UserEntity findByEmail(String email);
-    UserEntity findByUsername(String username);
+    UserEntity findByUsername(String username) throws UsernameNotFoundException;
 }

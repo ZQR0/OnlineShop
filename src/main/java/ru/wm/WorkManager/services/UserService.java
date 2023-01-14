8416e477335eccb252ec580e74c210f1bb2500e7
ZQR0 <yarikrech@gmail.com>
2023-01-14 22:22:52 +0300

    role setting in register service

--- src/main/java/ru/wm/WorkManager/services/UserService.java
+++ src/main/java/ru/wm/WorkManager/services/UserService.java
@@ -6,10 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.wm.WorkManager.dto.UserDTO;
+import ru.wm.WorkManager.entities.RoleEntity;
 import ru.wm.WorkManager.entities.UserEntity;
 import ru.wm.WorkManager.exceptions.EmailValidationException;
-import ru.wm.WorkManager.exceptions.UserNotFoundException;
 import ru.wm.WorkManager.repositories.UserRepository;
 import ru.wm.WorkManager.services.interfaces.UserServiceInterface;
 import ru.wm.WorkManager.utils.DateProvider;
@@ -20,6 +21,7 @@ import java.util.logging.Logger;
 
 @Service
 @Slf4j
+@Transactional
 public class UserService implements UserServiceInterface {
 
     @Autowired
@@ -55,14 +57,17 @@ public class UserService implements UserServiceInterface {
     public void register(UserDTO dto) throws EmailValidationException {
 
         if (validateEmail(dto.getEmail())) {
-            this.repository.save(
-                    UserEntity.builder()
-                            .setEmail(dto.getEmail())
-                            .setUsername(dto.getUsername())
-                            .setPassword(dto.getPassword())
-                            .date(this.dateProvider.now())
-                            .build()
-            );
+            if (!userExistsByEmail(dto.getEmail())) {
+                this.repository.save(
+                        UserEntity.builder()
+                                .setEmail(dto.getEmail())
+                                .setUsername(dto.getUsername())
+                                .setPassword(dto.getPassword())
+                                .date(this.dateProvider.now())
+                                .role(RoleEntity.USER.getRoleName())
+                                .build()
+                );
+            }
 
             Logger.getLogger("registerUser").info("User is registered");
         }
@@ -76,5 +81,9 @@ public class UserService implements UserServiceInterface {
         // Got this pattern from : https://www.baeldung.com/java-email-validation-regex
         //final String regexPattern = "^(?=.{1,64}@)[A-Za-z0-9\\\\+_-]+(\\\\.[A-Za-z0-9\\\\+_-]+)*@" + "[^-][A-Za-z0-9\\\\+-]+(\\\\.[A-Za-z0-9\\\\+-]+)*(\\\\.[A-Za-z]{2,})$";
         return EmailValidator.getInstance().isValid(email);
+    }
+
+    private boolean userExistsByEmail(String email) {
+        return this.findByEmail(email) != null;
     }
 }

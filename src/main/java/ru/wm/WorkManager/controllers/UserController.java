946f05948272df234040993b45877976d72a4d11
ZQR0 <yarikrech@gmail.com>
2023-01-14 20:47:00 +0300

    path and params changes in UserController

--- src/main/java/ru/wm/WorkManager/controllers/UserController.java
+++ src/main/java/ru/wm/WorkManager/controllers/UserController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.*;
 import ru.wm.WorkManager.dto.UserDTO;
 import ru.wm.WorkManager.entities.UserEntity;
 import ru.wm.WorkManager.exceptions.EmailValidationException;
-import ru.wm.WorkManager.exceptions.UserNotFoundException;
 import ru.wm.WorkManager.services.UserService;
 
 import java.util.List;
@@ -16,47 +15,52 @@ import java.util.Optional;
 @RestController
 @Slf4j
 public class UserController {
-
+    //TODO("User controller mappings")
     @Autowired
     private UserService service;
 
+
     @GetMapping(
-            path = "user/get-all-users",
+            path = "api/user/get-all-users",
             produces = MediaType.APPLICATION_JSON_VALUE
     )
     public List<UserEntity> getAllUsers() {
         return this.service.getAllUsers();
     }
 
+
     @GetMapping(
-            path = "user/by-id/",
-            params = "/{id}",
+            path = "api/user/by-id/",
+            params = "id",
             produces = MediaType.APPLICATION_JSON_VALUE
     )
     public Optional<UserEntity> getById(@RequestParam(name = "id") Long id) {
         return this.service.findById(id);
     }
 
+
     @GetMapping(
-            path = "user/by-username/",
-            params = {"/{username}"},
+            path = "api/user/by-username/",
+            params = "username",
             produces = MediaType.APPLICATION_JSON_VALUE
     )
     public UserEntity findByUsername(@RequestParam(name = "username") String username) {
         return this.service.findByUsername(username);
     }
 
+
     @GetMapping(
-            path = "user/by-email/",
-            params = "/{email}",
+            path = "api/user/by-email/",
+            params = "email",
             produces = MediaType.APPLICATION_JSON_VALUE
     )
     public UserEntity findByEmail(@RequestParam(name = "email") String email) {
         return this.service.findByEmail(email);
     }
 
+
     @PostMapping(
-            path = "user/register/",
+            path = "api/user/register/",
             produces = MediaType.APPLICATION_JSON_VALUE,
             consumes = MediaType.APPLICATION_JSON_VALUE
     )

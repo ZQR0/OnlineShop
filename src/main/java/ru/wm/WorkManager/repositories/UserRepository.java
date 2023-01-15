b3772935fad97ae1186a0ccbe11c41ce52b7aa1c
ZQR0 <yarikrech@gmail.com>
2023-01-15 16:29:37 +0300

    UserRepository methods types changed to Optional (as in findById method)

--- src/main/java/ru/wm/WorkManager/repositories/UserRepository.java
+++ src/main/java/ru/wm/WorkManager/repositories/UserRepository.java
@@ -3,8 +3,10 @@ package ru.wm.WorkManager.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.wm.WorkManager.entities.UserEntity;
 
+import java.util.Optional;
+
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
-    UserEntity findByEmail(String email);
-    UserEntity findByUsername(String username);
+    Optional<UserEntity> findByEmail(String email);
+    Optional<UserEntity> findByUsername(String username);
 }

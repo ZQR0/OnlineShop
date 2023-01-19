9582a28e3bf614876f23d57e73ce1cdcd70547d6
ZQR0 <yarikrech@gmail.com>
2023-01-19 21:45:13 +0300

    register method removed

--- src/main/java/ru/os/OnlineShop/services/UserService.java
+++ src/main/java/ru/os/OnlineShop/services/UserService.java
@@ -6,10 +6,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import ru.os.OnlineShop.dto.UserDTO;
-import ru.os.OnlineShop.entities.RoleEntity;
 import ru.os.OnlineShop.entities.UserEntity;
-import ru.os.OnlineShop.exceptions.EmailValidationException;
 import ru.os.OnlineShop.exceptions.UserNotFoundException;
 import ru.os.OnlineShop.repositories.UserRepository;
 import ru.os.OnlineShop.services.interfaces.UserServiceInterface;
@@ -17,8 +14,6 @@ import ru.os.OnlineShop.utils.DateProvider;
 import ru.os.OnlineShop.utils.ValidationComponent;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.logging.Logger;
 
 @Service
 @Slf4j
@@ -61,30 +56,5 @@ public class UserService implements UserServiceInterface {
         return this.repository.findByUsername(username).orElseThrow(
                 () -> new UsernameNotFoundException("User with username " + username + " not found")
         );
-    }
-
-    @Override
-    public void register(UserDTO dto) throws EmailValidationException {
-
-        if (this.validationComponent.validateEmail(dto.getEmail())) {
-            if (!this.validationComponent.userExistsByEmail(dto.getEmail())) {
-                // Saving user with builder
-                this.repository.save(
-                        UserEntity.builder()
-                                .setEmail(dto.getEmail())
-                                .setUsername(dto.getUsername())
-                                .setPassword(dto.getPassword())
-                                .date(this.dateProvider.now())
-                                .role(RoleEntity.USER.getRoleName())
-                                .build()
-                );
-            }
-
-            Logger.getLogger("registerUser").info("User is registered");
-        }
-
-        Logger.getLogger("registerUser").info("User isn't registered");
-
-        throw new EmailValidationException("Email validation failed");
     }
 }

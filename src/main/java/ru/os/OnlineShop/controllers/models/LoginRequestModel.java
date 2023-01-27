f9badf548a284151333d091526c25ffc77dc85b6
ZQR0 <yarikrech@gmail.com>
2023-01-27 23:06:45 +0300

    LoginRequestModel created for future auth requests

---
+++ src/main/java/ru/os/OnlineShop/controllers/models/LoginRequestModel.java
@@ -0,0 +1,16 @@
+package ru.os.OnlineShop.controllers.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+
+@Data
+@Slf4j
+public class LoginRequestModel {
+    @JsonProperty("email")
+    private String email;
+
+    @JsonProperty("password")
+    private String password;
+}

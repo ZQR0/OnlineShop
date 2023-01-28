77cf2aba66cdbb8bd75941d826eceb491e7c5fe3
ZQR0 <yarikrech@gmail.com>
2023-01-28 22:52:02 +0300

    rest auth entry point logging added

--- src/main/java/ru/os/OnlineShop/security/RestAuthEntryPoint.java
+++ src/main/java/ru/os/OnlineShop/security/RestAuthEntryPoint.java
@@ -3,6 +3,7 @@ package ru.os.OnlineShop.security;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -10,12 +11,13 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 
 @Component
+@Slf4j
 public class RestAuthEntryPoint implements AuthenticationEntryPoint {
 
     @Override
     public void commence(HttpServletRequest request,
                          HttpServletResponse response,
                          AuthenticationException authException) throws IOException, ServletException {
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
     }
 }

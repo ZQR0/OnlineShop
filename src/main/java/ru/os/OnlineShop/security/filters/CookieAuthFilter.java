255e12e3dea98d6a6a90fc5de173e43952f5e433
ZQR0 <yarikrech@gmail.com>
2023-01-29 21:01:12 +0300

    Cookie authentication filter added

--- src/main/java/ru/os/OnlineShop/security/filters/CookieAuthFilter.java
+++ src/main/java/ru/os/OnlineShop/security/filters/CookieAuthFilter.java
@@ -1,6 +1,7 @@
 package ru.os.OnlineShop.security.filters;
 
-import jakarta.servlet.*;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -13,48 +14,38 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Optional;
-import java.util.logging.Logger;
 import java.util.stream.Stream;
 
 /*
-* @author ZQR0
-* @since 28.01.2023
-* We need this component to filter all input cookies
-* and check if cookie already exists in our system
-*
-* Using @Slf4j for logging
-*/
+ * @author ZQR0
+ * @since 28.01.2023
+ * We need this component to filter all input cookies
+ * and check if cookie already exists in our system
+ *
+ * Using @Slf4j for logging
+ */
 @Component
 @Slf4j
 @PropertySource("classpath:application.properties")
-public class CookieAuthFilter implements Filter {
+public class CookieAuthFilter extends OncePerRequestFilter {
 
-
-    // This is still COOKIE_NAME, but not final, because this value is const in application.properties
     @Value(value = "${cookie.cookie-name}")
     public static String COOKIE_NAME;
 
-
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-
-        HttpServletRequest req = (HttpServletRequest) request;
-
-        Optional<Cookie> cookieAuth = Stream.of(Optional.ofNullable(req.getCookies()).orElse(new Cookie[0]))
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        Optional<Cookie> authCookie = Stream.of(Optional.ofNullable(request.getCookies()).orElse(new Cookie[0]))
                 .filter(cookie -> COOKIE_NAME.equals(cookie.getName()))
                 .findFirst();
 
-        cookieAuth.ifPresent(cookie -> SecurityContextHolder.getContext().setAuthentication(
-                new PreAuthenticatedAuthenticationToken(cookie.getValue(), null)
-        ));
+        authCookie.ifPresent(cookie -> SecurityContextHolder.getContext().setAuthentication(
+                new PreAuthenticatedAuthenticationToken(cookie.getValue(), null))
+        );
 
-        chain.doFilter(req, response);
-    }
-
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-        Filter.super.init(filterConfig);
-        Logger.getLogger("Cookie Auth Filter").info("Cookie Auth Filter is working");
+        filterChain.doFilter(request, response);
     }
 }

4a48ff2e6f0acc2a9747ecb44a0650ce7ab858b9
ZQR0 <yarikrech@gmail.com>
2023-02-07 22:00:51 +0300

    User Repository method findByUsername replaced by findByFirstName

--- src/main/java/ru/os/OnlineShop/repositories/UserRepository.java
+++ src/main/java/ru/os/OnlineShop/repositories/UserRepository.java
@@ -1,12 +1,14 @@
 package ru.os.OnlineShop.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.os.OnlineShop.entities.UserEntity;
 
 import java.util.Optional;
 
 
+@Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     Optional<UserEntity> findByEmail(String email);
-    Optional<UserEntity> findByUsername(String username);
+    Optional<UserEntity> findByFirstName(String username);
 }

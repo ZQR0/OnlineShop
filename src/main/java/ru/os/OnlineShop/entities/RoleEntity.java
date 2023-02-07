23032ec61016ab5a913533c94d64f5ef088371f5
ZQR0 <yarikrech@gmail.com>
2023-02-07 21:59:33 +0300

    Role Entity code refactored

--- src/main/java/ru/os/OnlineShop/entities/RoleEntity.java
+++ src/main/java/ru/os/OnlineShop/entities/RoleEntity.java
@@ -1,17 +1,8 @@
 package ru.os.OnlineShop.entities;
 
+
+
 public enum RoleEntity {
-    ADMIN("ADMIN"),
-    USER("USER"),
-    BANNED("BANNED");
-
-    private final String _roleName;
-
-    RoleEntity(String roleName) {
-        this._roleName = roleName;
-    }
-
-    public String getRoleName() {
-        return this._roleName;
-    }
+    ADMIN,
+    USER;
 }

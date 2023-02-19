5118873702f7121c4c524ecd886819ba27b8faaa
ZQR0 <yarikrech@gmail.com>
2023-02-19 18:34:41 +0300

    JwtService logging added

--- src/main/java/ru/os/OnlineShop/services/JwtService.java
+++ src/main/java/ru/os/OnlineShop/services/JwtService.java
@@ -29,22 +29,26 @@ public class JwtService implements JwtServiceInterface {
 
     @Override
     public String extractUsername(String token) {
+        log.info("extractUsername works");
         return extractClaim(token, Claims::getSubject);
     }
 
     @Override
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
+        log.info("extractClaims works");
         return claimsResolver.apply(claims);
     }
 
     @Override
     public String getGeneratedToken(UserDetails userDetails) {
+        log.info("getGeneratedToken works");
         return this.generateToken(new HashMap<>(), userDetails);
     }
 
     @Override
     public String generateToken(HashMap<String, Object> extraClaims, UserDetails userDetails) {
+        log.info("generateToken works");
         return Jwts.builder()
                 .setClaims(extraClaims)
                 .setSubject(userDetails.getUsername())
@@ -57,31 +61,36 @@ public class JwtService implements JwtServiceInterface {
     @Override
     public boolean isTokenValid(String token, UserDetails userDetails) {
         final String username = extractUsername(token);
+        log.info("isTokenValid works");
         return (username.equals(userDetails.getUsername())) && isTokenExpired(token);
     }
 
     @Override
     public boolean isTokenExpired(String token) {
+        log.info("isTokenExpired works");
         return this.extractExpiration(token).before(new Date());
     }
 
     @Override
     public Date extractExpiration(String token) {
+        log.info("extractExpiration works");
         return this.extractClaim(token, Claims::getExpiration);
     }
 
     @Override
     public Claims extractAllClaims(String token) {
+        log.info("extractAllClaims works");
         return Jwts.parserBuilder()
                 .setSigningKey(this.getSignInKey())
                 .build()
-                .parseClaimsJwt(token)
+                .parseClaimsJws(token)
                 .getBody();
     }
 
     @Override
     public Key getSignInKey() {
         byte[] keyBytes = Decoders.BASE64.decode(this.JWT_SECRET_KEY);
+        log.info("getSignInKey works");
         return Keys.hmacShaKeyFor(keyBytes);
     }
 }

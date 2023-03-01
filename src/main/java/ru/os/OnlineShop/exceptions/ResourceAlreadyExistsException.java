5eeb7736e72a626bfda8c8318ddf608163a9c4aa
ZQR0 <yarikrech@gmail.com>
2023-03-01 22:05:56 +0300

    new exception

---
+++ src/main/java/ru/os/OnlineShop/exceptions/ResourceAlreadyExistsException.java
@@ -0,0 +1,8 @@
+package ru.os.OnlineShop.exceptions;
+
+public class ResourceAlreadyExistsException extends Exception{
+
+    public ResourceAlreadyExistsException(String msg) {
+        super(msg);
+    }
+}
